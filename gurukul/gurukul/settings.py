ea92f6c27ee3bb598c3bc30d1921f16d35834f60
Mahendra Meena <137411499+mahendrameena15@users.noreply.github.com>
2023-07-31 19:08:38 +0530

    Update settings.py

--- gurukul/gurukul/settings.py
+++ gurukul/gurukul/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ad(bc)#el)@skx_%(ravc%c#4hwkc$2h4-!nbf=q+jf0#f%zz1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
