0aa7511c7e7dc0e216c104d6194cfd06434d6a6a
Mahendra Meena <137411499+mahendrameena15@users.noreply.github.com>
2023-07-31 19:37:29 +0530

    Update settings.py

--- gurukul/gurukul/settings.py
+++ gurukul/gurukul/settings.py
@@ -52,7 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
 ]
+CORS_ALLOWED_ORIGINS = ["https://sgbveda.org", ]
 
 ROOT_URLCONF = 'gurukul.urls'
 
